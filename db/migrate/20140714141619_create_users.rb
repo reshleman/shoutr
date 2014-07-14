4ba144d7e7b4f7d29cbcc54973c3d88fbc6bd313
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-14 10:50:06 -0400

    Create users table w/ username

--- db/migrate/20140714141619_create_users.rb
+++ db/migrate/20140714141619_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :username, null: false
       t.string :email, null: false
       t.string :password_digest, null: false
 
@@ -8,5 +9,6 @@ class CreateUsers < ActiveRecord::Migration
     end
 
     add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
   end
 end
