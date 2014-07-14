c6d4b179d2f1907967164e04b7b8122680607085
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-14 14:34:43 -0400

    Implement shouting and viewing shouts
    * Add form to dashboards#show for creating a shout
    * Add route for shouts#create
    * Add shouts controller and implement shotus#create
    * Display the current user's shouts on dashboards#show, using a partial

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,21 @@
+class ShoutsController < ApplicationController
+  def create
+    user = current_user
+    shout_params_with_user = shout_params.merge(user_id: user.id)
+    @shout = Shout.new(shout_params_with_user)
+
+    if @shout.save
+      redirect_to root_path
+    else
+      render "dashboards/show"
+    end
+  end
+
+  private
+
+  def shout_params
+    params.
+      require(:shout).
+      permit(:content)
+  end
+end
