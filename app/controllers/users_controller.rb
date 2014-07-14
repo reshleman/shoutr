e0057e996c9300330e4e1ad2519b6106bb6bc5d1
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-14 17:42:01 -0400

    Display username in shouts
    * Update shout partial to display username
    * Use vanity paths for users#show pages
    * Link to users#show from shout
    * Override to_param for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(username: params[:id])
+    @shouts = @user.shouts
   end
 
   private
