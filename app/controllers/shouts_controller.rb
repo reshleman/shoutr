4f707a7314e3fdc94d2679f825371c05ff3cadbf
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-14 16:37:08 -0400

    Refactor shouts#create

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,11 +1,7 @@
 class ShoutsController < ApplicationController
   def create
-    user = current_user
-    shout_params_with_user = shout_params.merge(user_id: user.id)
-    @shout = Shout.new(shout_params_with_user)
-
-    if @shout.save
-      redirect_to root_path
+    if current_user.shouts.create(shout_params)
+      redirect_to dashboard_path
     else
       @shouts = current_user.shouts
       render "dashboards/show"
