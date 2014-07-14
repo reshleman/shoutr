02b341f416eb05a486f6e0dc77dd413e9cc0b5cf
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-14 16:49:40 -0400

    Add users#show page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   root "homes#show"
 
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :shouts, only: [:create]
 end
