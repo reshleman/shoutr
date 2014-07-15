9501823b46e21541cb83edf27b6cd1a2beeee14b
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-15 16:21:41 -0400

    Add uniqueness validation to FollowingRelationship

--- app/models/following_relationship.rb
+++ app/models/following_relationship.rb
@@ -1,4 +1,6 @@
 class FollowingRelationship < ActiveRecord::Base
   belongs_to :follower, class_name: "User"
   belongs_to :followed_user, class_name: "User"
+
+  validates :follower_id, uniqueness: { scope: :followed_user_id }
 end
