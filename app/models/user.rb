fbf86168a20490205824028ba3f4bc1832cef549
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-07-16 13:37:04 -0400

    Refactor User model for clarity

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,21 @@ class User < ActiveRecord::Base
   has_many :shouts
 
   has_many :followed_user_relationships,
-            class_name: "FollowingRelationship",
-            foreign_key: :follower_id
+    class_name: "FollowingRelationship",
+    foreign_key: :follower_id
 
   has_many :followed_users, through: :followed_user_relationships
 
   has_many :follower_relationships,
-            class_name: "FollowingRelationship",
-            foreign_key: :followed_user_id
+    class_name: "FollowingRelationship",
+    foreign_key: :followed_user_id
 
   has_many :followers, through: :follower_relationships
 
   def timeline
-    Shout.where(user_id: [id] + followed_user_ids).order(created_at: :desc)
+    Shout.
+      where(user_id: self_and_followed_users_ids).
+      order(created_at: :desc)
   end
 
   def follow(user)
@@ -34,5 +36,11 @@ class User < ActiveRecord::Base
 
   def to_param
     username
+  end
+
+  private
+
+  def self_and_followed_users_ids
+    [id] + followed_user_ids
   end
 end
